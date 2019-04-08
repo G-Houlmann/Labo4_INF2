4d0426f6c662b6b03c59651a435a181ab2955958
G-Houlmann <gildas.houlmann@gmail.com>
2019-04-08 18:01:11 +0200

    vecteur fini et exceptions commencées

--- Project1/Project1/Exceptions.h
+++ Project1/Project1/Exceptions.h
@@ -1,6 +1,28 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
+#include <string>
+#include <stdexcept>
+#include <exception>
 
-class hors_limite {};
+class index_hors_limite : public std::logic_error {
+public:
+	explicit index_hors_limite(const std::string& s) : std::logic_error(s) {}
+	explicit index_hors_limite(const char* s) : std::logic_error(s) {}
+};
+
+class taille_trop_haute : public std::logic_error {
+public:
+	explicit taille_trop_haute(const std::string& s) : std::logic_error(s) {}
+	explicit taille_trop_haute(const char* s) : std::logic_error(s) {}
+};
+
+class taille_vecteurs_incompatibles : public std::logic_error {
+public:
+	explicit taille_vecteurs_incompatibles(const std::string& s) : std::logic_error(s) {}
+	explicit taille_vecteurs_incompatibles(const char* s) : std::logic_error(s) {}
+};
+
+
+
 
 #endif // !EXCEPTIONS_H
